fdeecacc7fdd3e60f8f01425bc68cd4acf1ae602
Omer <om.yizhaq@gmail.com>
2020-12-30 21:35:31 +0200

    Making mirror_pair not take self edge and or nonexistant nodes

--- graph.py
+++ graph.py
@@ -1,25 +1,38 @@
-import networkx as nx 
+import networkx as nx
 import matplotlib.pyplot as plt
 import csv
 import pandas as pd
 import random
 from datetime import datetime
+import scipy.sparse as sp
+import numpy as np
 
 TSV_FIELDS = ["FromNodeId", "ToNodeId", "Sign"]
 
+
 # TODO CHANGE TO WORK WITH PANDAS
-def build_graph(tsv):
+def build_graph(tsv, record_fake_data=False):
     graph = nx.DiGraph()
     tsv_file = open(tsv, "r")
     df = pd.read_csv(tsv, sep="\t")
     # skip headers line:
     next(tsv_file)
+
+    real_nodes = set()
+
     for i in range(df.max().max()):
         if i not in graph.nodes():
             graph.add_node(i)
     for line in csv.reader(tsv_file, delimiter="\t"):
-        graph.add_edge(int(line[0]), int(line[1]), weight = int(line[2]))
-    return graph
+        graph.add_edge(int(line[0]), int(line[1]), weight=int(line[2]))
+        real_nodes.add(int(line[0]))
+        real_nodes.add(int(line[1]))
+
+    if record_fake_data:
+        return graph, real_nodes
+    else:
+        return graph
+
 
 def graph_to_tsv(graph, output_file_name):
     with open(output_file_name, "w", newline="") as tsvfile:
@@ -28,8 +41,9 @@ def graph_to_tsv(graph, output_file_name):
 
         writer.writeheader()
 
-        for (u,v) in graph.edges():
+        for (u, v) in graph.edges():
             writer.writerow({TSV_FIELDS[0]: u, TSV_FIELDS[1]: v, TSV_FIELDS[2]: graph[u][v]["weight"]})
+
 
 def create_graph_variants(tsv, name):
     type1graph = build_graph(tsv)
@@ -42,7 +56,7 @@ def create_graph_variants(tsv, name):
     num_negative_edges = 0
     random_positive_edges = []
 
-    for (u,v) in type1graph.edges():
+    for (u, v) in type1graph.edges():
         if type1graph[u][v]["weight"] == -1:
             negative_edges.append((u, v))
             num_negative_edges = num_negative_edges + 1
@@ -62,9 +76,9 @@ def create_graph_variants(tsv, name):
 
 
 def draw_graph(graph):
-    pos = nx.spring_layout(graph)   
-    plt.figure(30, figsize=(30,30))
-    nx.draw(graph, pos, node_size=1500, font_size = 10, with_labels=True)
+    pos = nx.spring_layout(graph)
+    plt.figure(30, figsize=(30, 30))
+    nx.draw(graph, pos, node_size=1500, font_size=10, with_labels=True)
     nx.draw_networkx_edge_labels(graph, pos, edge_labels=nx.get_edge_attributes(graph, 'weight'))
 
 
@@ -81,4 +95,3 @@ if __name__ == "__main__":
     t2 = datetime.now()
     triads_time = t2 - t1
     print(triads_time)
-
--- predictor.py
+++ predictor.py
@@ -14,20 +14,42 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 
 
-def mirror_pair(pair, embeddedness_matrix, adj, marker):
+def mirror_pair(pair, embeddedness_matrix, adj, existance_mat, marker):
     embeddedness = embeddedness_matrix[pair]
-    equal_pair = next(
-        zip(*np.where(functools.reduce(np.logical_and, (embeddedness_matrix == embeddedness, adj == 0, marker == 0)))))
-    if equal_pair is not None:
-        marker[equal_pair] = 1
-    return equal_pair
+    # find possible mirror pairs, that should be a. of the same embeddedness, b. unconnected, c. not already chosen
+    candidates = zip(*np.where(
+        functools.reduce(np.logical_and,
+                         (embeddedness_matrix == embeddedness, adj == 0, existance_mat == 1, marker == 0)))
+    )
+
+    candidate = next(candidates, None)
+    if candidate:
+        marker[candidate] = 1
+    return candidate
+
+
+def calc_existence_mat(nodes):
+    _zs = np.zeros(max(nodes) + 1, dtype=int)
+
+    for i in nodes:
+        _zs[i] = 1
+    _zs = _zs[:, np.newaxis]
+
+    # the result of this matrix mul will yield 1 only in cells [i, j]
+    # where both Node i and j are ones, meaning, they both exists in the graph
+    existence_mat = _zs * _zs.T
+
+    # Since we don't need the diagonal, we can zero it.
+    np.fill_diagonal(existence_mat, 0)
+    return existence_mat
 
 
 def split_data_balanced(features_file, tsv_file):
     features = pd.read_csv(features_file, sep="\t")
 
     # calculate adj matrix
-    G = helper.build_graph(tsv_file)
+    G, real_nodes = helper.build_graph(tsv_file, record_fake_data=True)
+
     A = sparse.csr_matrix(nx.to_numpy_matrix(G, weight="weight"))
 
     # Matrix M[i,j] showing adjacency between Node i and j
@@ -39,11 +61,14 @@ def split_data_balanced(features_file, tsv_file):
     embeddedness_matrix = (B @ B.T).todense()
     # Marks the nonedges already chosen
     marker = np.zeros(A.shape)
+    # Matrix M[i,j] marks whether both Nodes i and j exists in the graph
+    existance_mat = calc_existence_mat(real_nodes)
 
     pos = list(zip(*np.where(adj > 0)))
     random.shuffle(pos)
     test_pos = pos[:len(pos) // 10]
-    mirrors = [mirror_pair(p, embeddedness_matrix, adj, marker) for p in test_pos]
+    mirrors = filter(lambda p: p is not None,
+                     [mirror_pair(p, embeddedness_matrix, adj, existance_mat, marker) for p in test_pos])
 
     # features = features.set_index('Unnamed: 0')
     test = [*test_pos, *mirrors]
@@ -75,30 +100,30 @@ if __name__ == "__main__":
     print("K")
     print(predict('./features-100-refactored.tsv', './datasets/wiki-demo-100.tsv'))
 
-    print("var1")
-    t1 = time_of_start_computation = datetime.now()
-
-    with open('out-wiki-ml-varient1.tsv', 'w') as f:
-        print(predict('./calculated_features/wiki-full-features.tsv'), file=f)
-
-    t2 = datetime.now()
-    triads_time = t2 - t1
-    print(triads_time)
-
-    print("var2")
-    t1 = time_of_start_computation = datetime.now()
-    with open('out-wiki-ml-varient2.tsv', 'w') as f:
-        print(predict('./calculated_features/wiki-varient2-full-features.tsv', ), file=f)
-
-    t2 = datetime.now()
-    triads_time = t2 - t1
-    print(triads_time)
-
-    print("var3")
-    t1 = time_of_start_computation = datetime.now()
-    with open('out-wiki-ml-varient3.tsv', 'w') as f:
-        print(predict('./calculated_features/wiki-varient3-full-features.tsv'), file=f)
-
-    t2 = datetime.now()
-    triads_time = t2 - t1
-    print(triads_time)
+    # print("var1")
+    # t1 = time_of_start_computation = datetime.now()
+    #
+    # with open('out-wiki-ml-varient1.tsv', 'w') as f:
+    #     print(predict('./calculated_features/wiki-full-features.tsv'), file=f)
+    #
+    # t2 = datetime.now()
+    # triads_time = t2 - t1
+    # print(triads_time)
+    #
+    # print("var2")
+    # t1 = time_of_start_computation = datetime.now()
+    # with open('out-wiki-ml-varient2.tsv', 'w') as f:
+    #     print(predict('./calculated_features/wiki-varient2-full-features.tsv', ), file=f)
+    #
+    # t2 = datetime.now()
+    # triads_time = t2 - t1
+    # print(triads_time)
+    #
+    # print("var3")
+    # t1 = time_of_start_computation = datetime.now()
+    # with open('out-wiki-ml-varient3.tsv', 'w') as f:
+    #     print(predict('./calculated_features/wiki-varient3-full-features.tsv'), file=f)
+    #
+    # t2 = datetime.now()
+    # triads_time = t2 - t1
+    # print(triads_time)
