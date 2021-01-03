0263a6be5688c4cbee338b163712acc28f39bb25
Omer <om.yizhaq@gmail.com>
2021-01-03 22:14:16 +0200

    Add documantation to graph.py

--- graph.py
+++ graph.py
@@ -4,13 +4,19 @@ import csv
 import pandas as pd
 import random
 from datetime import datetime
-import scipy.sparse as sp
-import numpy as np
+
 
 TSV_FIELDS = ["FromNodeId", "ToNodeId", "Sign"]
 
 
-# TODO CHANGE TO WORK WITH PANDAS
+""" Builds a directed graph based on the information in the tsv file given.
+    As the basis of all runtime optimizations performed, the graph will be represented as adjacency matrix A in which
+    cell A[i][j] = edge sign between i to j. Thus, nodes which are not existing in the graph were added to it to fill 
+    the spaces between existing nodes. 
+    The wikipedia graph contains about 1000 non-real nodes and 7000 nodes.
+    The existing nodes are being recorded for correct future use (features building, selecting non edges from the graph
+    for 10-fold varient). 
+"""
 def build_graph(tsv, record_fake_data=False):
     graph = nx.DiGraph()
     tsv_file = open(tsv, "r")
@@ -23,6 +29,7 @@ def build_graph(tsv, record_fake_data=False):
     for i in range(df.max().max()):
         if i not in graph.nodes():
             graph.add_node(i)
+
     for line in csv.reader(tsv_file, delimiter="\t"):
         graph.add_edge(int(line[0]), int(line[1]), weight=int(line[2]))
         real_nodes.add(int(line[0]))
@@ -34,6 +41,7 @@ def build_graph(tsv, record_fake_data=False):
         return graph
 
 
+# This function is used to create the 3 additional graph variants from the original graph
 def graph_to_tsv(graph, output_file_name):
     with open(output_file_name, "w", newline="") as tsvfile:
         fieldnames = TSV_FIELDS
@@ -44,13 +52,21 @@ def graph_to_tsv(graph, output_file_name):
         for (u, v) in graph.edges():
             writer.writerow({TSV_FIELDS[0]: u, TSV_FIELDS[1]: v, TSV_FIELDS[2]: graph[u][v]["weight"]})
 
-
+""" There are 4 graph vairants:
+    Variant 1: the original graph
+    Variant 2: the graph without all negative edges N 
+    Variant 3: the graph without all negative edges and without a group of randonely selected positive edges P+ such
+               that |N| = |P+|
+    Variant 4: the graph without all edges from the group P+
+    
+    All varients are being written to tsv files for future computation of features 
+"""
 def create_graph_variants(tsv, name):
     type1graph = build_graph(tsv)
     type2graph = type1graph.copy()
     type3graph = type1graph.copy()
     type4graph = type1graph.copy()
-    print("HI")
+
     negative_edges = []
     positive_edges = []
     num_negative_edges = 0
@@ -74,7 +90,7 @@ def create_graph_variants(tsv, name):
     graph_to_tsv(type3graph, "variant3-" + name)
     graph_to_tsv(type4graph, "variant4-" + name)
 
-
+# This one was just for fun
 def draw_graph(graph):
     pos = nx.spring_layout(graph)
     plt.figure(30, figsize=(30, 30))
