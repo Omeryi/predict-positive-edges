4900ff20e8dd09c9d16247957c7b92d31ebc76b5
Omer <om.yizhaq@gmail.com>
2021-01-10 22:02:10 +0200

    Some stuff

--- predictor.py
+++ predictor.py
@@ -1,7 +1,9 @@
+import contextlib
 import functools
 import random
 from collections import Counter
 import multiprocessing as mp
+import re
 
 import networkx as nx
 import numpy as np
@@ -9,17 +11,22 @@ import pandas as pd
 from scipy import sparse
 from sklearn.metrics import classification_report
 from xgboost import XGBClassifier
+import lightgbm as lgbm
 
 import graph as helper
 import logging
 
 logging.basicConfig(
+    #filename='prediction_logs1',
+    #filemode='a',
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
 
 
+# This function returns a list of non-existing nodes with the same embeddedness in order to compile the 10 test chunks
 def mirror_pair(pairs, embeddedness_matrix, adj, existance_mat, marker):
+    # count all embeddedness values the the function should return
     embeddedness_hist = Counter(embeddedness_matrix[tuple(pair)] for pair in pairs)
     # find possible mirror pairs, that should be a. of the same embeddedness_hist, b. unconnected, c. not already chosen
     candidates = zip(*np.where(
@@ -38,7 +45,10 @@ def mirror_pair(pairs, embeddedness_matrix, adj, existance_mat, marker):
         )
     ))
 
+    # Iterate through all potential mirror pairs, until all mirror pairs are found
     for candidate in candidates:
+
+        # All mirror pairs needed were found
         if not embeddedness_hist:
             return
 
@@ -46,10 +56,11 @@ def mirror_pair(pairs, embeddedness_matrix, adj, existance_mat, marker):
 
         if candidate_embeddedness in embeddedness_hist and embeddedness_hist[candidate_embeddedness] > 0:
             embeddedness_hist[candidate_embeddedness] -= 1
+            if embeddedness_hist[candidate_embeddedness] == 0:
+                del embeddedness_hist[candidate]
+
             marker[candidate] = 1
             yield candidate
-        elif candidate_embeddedness in embeddedness_hist:
-            del embeddedness_hist[candidate]
 
 
 def calc_existence_mat(nodes):
@@ -69,10 +80,10 @@ def calc_existence_mat(nodes):
 
 
 def split_data_balanced(features_file, tsv_file):
-    logging.info("reading features")
-    features = pd.read_csv(features_file, sep="\t")
+    logger.info("reading features")
+    features = pd.read_csv(features_file, sep="\t").rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', x))
 
-    logging.info("preprocessing")
+    logger.info("preprocessing")
     # calculate adj matrix
     G, real_nodes = helper.build_graph(tsv_file, record_fake_data=True)
 
@@ -90,80 +101,117 @@ def split_data_balanced(features_file, tsv_file):
     # Matrix M[i,j] marks whether both Nodes i and j exists in the graph
     existance_mat = calc_existence_mat(real_nodes)
 
-    logging.info("get positives")
+    logger.info("get positives")
     X, y = features.drop('edge_sign', axis=1), features['edge_sign']
     positives = list(zip(*np.where(adj > 0)))
     random.shuffle(positives)
     splitter = []
 
     for idx, test_chunk in enumerate(np.array_split(positives, 10)):
-        logging.info(f"mirroring {idx + 1}")
+        logger.info(f"mirroring {idx + 1}")
         mirrors = mirror_pair(test_chunk, embeddedness_matrix, adj, existance_mat, marker)
         curr_test_vals = map(tuple, [*test_chunk, *mirrors])
 
-        logging.info(f"slicing {idx + 1}")
-        curr_test = features[features['Unnamed: 0'].isin([str(t) for t in curr_test_vals])]
+        logger.info(f"slicing {idx + 1}")
+        curr_test = features[features['K'].isin([str(t) for t in curr_test_vals])]
         curr_train = features.drop(curr_test.index)
         splitter.append((list(curr_train.index), list(curr_test.index)))
-    return X.drop(['Unnamed: 0'], axis=1), y, splitter
+    return X.drop(['K'], axis=1), y, splitter
 
 
-def predict_splice(train, test, X, y , idx):
-    logging.info(f"classifying {idx + 1}")
-    logmodel = XGBClassifier(n_jobs=-1)
+def predict_splice(train, test, X, y, idx):
+    parameters = {
+        # 'objective': 'binary',
+        # 'metric': 'auc',
+        'is_unbalance': 'true',
+        # 'feature_fraction': 0.5,
+        # 'bagging_fraction': 0.5,
+        # 'bagging_freq': 20,
+        # 'num_threads': 2,
+        # 'seed': 76
+    }
+
+    logger.info(f"classifying {idx + 1}")
+
+    logmodel = lgbm.LGBMClassifier()
+    #logmodel = XGBClassifier()
     logmodel.fit(X.loc[train], y.loc[train])
-    prediction = logmodel.predict(X.loc[test])
-    logging.info(f"postprocessing {idx + 1}")
+    prediction = logmodel.predict(parameters, X.loc[test])
+    with open(f'C:/Users/omyiz/Documents/repos/positive_edges_prediction/xxxxxx.txt', 'w') as xx:
+        xx.write(classification_report(y.loc[test], prediction))
+    logger.info(f"postprocessing {idx + 1} XXXXXXXX")
     return y.loc[test], prediction, test
 
 
 def predict(features_file, tsv_file):
-    data = pd.read_csv(features_file, sep="\t").rename(columns={'Unnamed: 0': 'K'})  # load data set
+    data = pd.read_csv(features_file, sep="\t").rename(columns={'Unnamed: 0': 'K'})
+    #data.rename(columns=lambda x: re.sub('[^A-Za-z0-9_]+', '', x))
+        # .drop(['positive_in_degree(u)',
+        #                 'positive_out_degree(u)', 'negative_in_degree(u)', 'negative_out_degree(u)',
+        #                 'positive_in_degree(v)', 'positive_out_degree(v)', 'negative_in_degree(v)',
+        #                 'negative_out_degree(v)', 'total_out_degree(u)',
+        #                 'total_in_degree(v)', 'C(u,v)', 'edge_sign'], axis=1)  # load data set
     data = data[(data.T != 0).any()]
-    data = data.drop('K', axis=1)
+    #data = data.drop('K', axis=1)
 
-    logging.info("splitting")
+    logger.info("splitting")
     X, y, splitter = split_data_balanced(features_file, tsv_file)
 
     labels = []
     preds = []
-    pool = mp.Pool(10)
-    results = [pool.apply(predict_splice, args=(train, test, X, y, idx)) for idx, (train, test) in enumerate(splitter)]
-    pool.close()
-
-    for label, predictions, test in results:
+    # pool = mp.Pool(10)
+    # results = [pool.apply(predict_splice, args=(train, test, X, y, idx)) for idx, (train, test) in enumerate(splitter)]
+    # pool.close()
+    #
+    # for label, predictions, test in results:
+    #     labels.extend(list(y.loc[test]))
+    #     preds.extend(list(predictions))
+    #
+    # print(classification_report(labels, preds))
+    for idx, (train, test) in enumerate(splitter):
+        logger.info(f"classifying {idx + 1}")
+        logmodel = lgbm.LGBMClassifier()
+        #logmodel = XGBClassifier(n_jobs=20)
+        logmodel.fit(X.loc[train], y.loc[train])
+        predictions = logmodel.predict(X.loc[test])
+        logger.info(f"postprocessing {idx + 1}")
+        logger.info(classification_report(list(y.loc[test]), list(predictions)))
         labels.extend(list(y.loc[test]))
         preds.extend(list(predictions))
 
-    print(classification_report(labels, preds))
-    # for idx, (train, test) in enumerate(splitter):
-    #     logging.info(f"classifying {idx + 1}")
-    #     logmodel = XGBClassifier(n_jobs=-1)
-    #     logmodel.fit(X.loc[train], y.loc[train])
-    #     predictions = logmodel.predict(X.loc[test])
-    #     logging.info(f"postprocessing {idx + 1}")
-    #     labels.extend(list(y.loc[test]))
-    #     preds.extend(list(predictions))
-    # print(classification_report(labels, preds))
+    return classification_report(labels, preds)
+
+
+@contextlib.contextmanager
+def timer():
+    import time
+
+    start = time.time()
+    yield
+    end = time.time()
+    print(end - start)
 
 
 if __name__ == "__main__":
-    logging.info("starting")
-    predict('./calculated_features/features-1000-refactored.tsv', './datasets/wiki-demo-1000.tsv')
+    logger = logging.getLogger()
+    logger.info("starting")
 
-    logging.info("starting v1")
+    with open('100-predict.tsv', 'w') as f:
+        print(predict('./calculated_features/wiki-features-1000-lines.txt', './datasets/wiki-demo-1000.tsv'), file=f)
 
-    with open('out-wiki-ml-variant1.tsv', 'w') as f:
-        print(predict('./calculated_features/wiki-full-features.tsv', './datasets/variant1-wiki.tsv'), file=f)
-
-    logging.info("starting v2")
-    with open('out-wiki-ml-variant2.tsv', 'w') as f:
-        print(predict('./calculated_features/wiki-variant2-full-features.tsv', './datasets/variant2-wiki.tsv'), file=f)
-
-    logging.info("starting v3")
-    with open('out-wiki-ml-variant3.tsv', 'w') as f:
-        print(predict('./calculated_features/wiki-variant3-full-features.tsv', './datasets/variant3-wiki.tsv'), file=f)
-
-    logging.info("starting v4")
-    with open('out-wiki-ml-variant4.tsv', 'w') as f:
-        print(predict('./calculated_features/wiki-variant4-full-features.tsv', './datasets/variant4-wiki.tsv'), file=f)
+    # logger.info("starting v1")
+    #
+    # with open('out-wiki-ml-variant1.tsv', 'w') as f:
+    #     print(predict('./calculated_features/var1-features.tsv', './datasets/variant1-wiki.tsv'), file=f)
+    #
+    # logger.info("starting v2")
+    # with open('out-wiki-ml-variant2.tsv', 'w') as f:
+    #     print(predict('./calculated_features/var2-features.tsv', './datasets/variant2-wiki.tsv'), file=f)
+    #
+    # logger.info("starting v3")
+    # with open('out-wiki-ml-variant3.tsv', 'w') as f:
+    #     print(predict('./calculated_features/var3-features.tsv', './datasets/variant3-wiki.tsv'), file=f)
+    #
+    # logger.info("starting v4")
+    # with open('out-wiki-ml-variant4.tsv', 'w') as f:
+    #     print(predict('./calculated_features/var4-features.tsv', './datasets/variant4-wiki.tsv'), file=f)
